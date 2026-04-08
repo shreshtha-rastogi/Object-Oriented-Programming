74bc14c0997548b6686a1ac004abfef097d694c5
shreshtha-rastogi <786shreshtharastogi@gmail.com>
2026-04-08 22:42:19 +0530

    add

--- 'templates/cmd_line.cpp
+++ 'templates/cmd_line.cpp
@@ -1,16 +1,13 @@
 #include <iostream>
 #include <fstream>
 using namespace std;
-
 int main(int argc, char *argv[]) {
     // Check if filename diya hai ya nahi
     if (argc != 2) {
         cout << "Usage: ./a.out filename\n";
         return 0;
     }
-
     ifstream file(argv[1]);  // File open kar rahe
-
     if (!file) {
         cout << "File not found!\n";
         return 0;
