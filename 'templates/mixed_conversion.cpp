5b2625ff128c3bb5263b9019e81e8a4b317aed85
shreshtha-rastogi <786shreshtharastogi@gmail.com>
2026-04-17 22:09:43 +0530

    add

---
+++ 'templates/mixed_conversion.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+class B;   // forward declaration
+class A{
+    public:
+        int x;
+        A(int a)
+        {
+            x = a;
+        }
+        // declare conversion function
+        operator B();
+};
+class B{
+    public:
+        int y;
+        B(){}
+        // constructor for conversion
+        B(A obj)
+        {
+            y = obj.x;
+        }
+};
+// define conversion function AFTER B is fully known
+A::operator B()
+{
+    B objb;
+    objb.y = x;
+    return objb;
+}
+int main()
+{
+    A obja(10);
+    // 🔹 Conversion using constructor
+    B objb1 = obja;
+    // objb2 = obja;
+    cout << objb1.y;
+}
